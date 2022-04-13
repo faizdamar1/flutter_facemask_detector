3b7b30ffc18ebb03778ea55df2b1e4ca122d333a
faizdamar1 <faizdamar123@gmail.com>
2022-04-13 13:42:55 +0700

    set available cameras

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:facemask_detector/main.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,10 +11,37 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   CameraImage? imgCamera;
-  CameraController? cameraController;
+  CameraController cameraController =
+      CameraController(cameras![0], ResolutionPreset.medium);
+
+  bool isWorking = false;
+  String result = '';
+
+  initCamera() {
+    cameraController.initialize().then((value) {
+      if (!mounted) return;
+
+      setState(() {
+        cameraController.startImageStream((image) {
+          if (!isWorking) {
+            isWorking = true;
+            imgCamera = image;
+          }
+        });
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    initCamera();
+  }
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
   }
 
   @override
@@ -23,14 +51,22 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(20),
         child: ListView(
           // crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            SizedBox(height: 50),
-            Text("Faiz Project"),
-            SizedBox(height: 5),
-            Text(
+          children: [
+            const SizedBox(height: 50),
+            const Text("Faiz Project"),
+            const SizedBox(height: 5),
+            const Text(
               "Facemask Detector",
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
+            (cameraController.value.isInitialized)
+                ? const Center(
+                    child: Icon(Icons.camera_alt_rounded),
+                  )
+                : AspectRatio(
+                    aspectRatio: cameraController.value.aspectRatio,
+                    child: CameraPreview(cameraController),
+                  )
           ],
         ),
       ),
